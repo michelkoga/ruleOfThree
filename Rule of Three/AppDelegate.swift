e71bd114a5161f0736514af08526d40aef5f1f72
Michel Koga <koga.michel@gmail.com>
2018-07-17 09:07:10 +0900

    Initial Commit

---
+++ Rule of Three/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Rule of Three
+//
+//  Created by 古賀ミッシェル on 2018/07/17.
+//  Copyright © 2018 古賀ミッシェル. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Rule of Three/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Rule of Three
+//
+//  Created by 古賀ミッシェル on 2018/07/17.
+//  Copyright © 2018 古賀ミッシェル. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
