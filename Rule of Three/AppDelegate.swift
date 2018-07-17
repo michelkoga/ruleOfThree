78f54f234c16d680da1d666bd7b031f040975aef
Michel Koga <koga.michel@gmail.com>
2018-07-17 10:48:33 +0900

    good

--- Rule of Three/AppDelegate.swift
+++ Rule of Three/AppDelegate.swift
@@ -11,16 +11,39 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
+	let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+	let popover = NSPopover()
+	
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
-		// Insert code here to initialize your application
+		if let button = statusItem.button {
+			NSEvent.addGlobalMonitorForEvents(matching: .leftMouseDown) {_ in
+				self.popover.close()
+			}
+			button.image = NSImage(named: NSImage.Name(rawValue: "ruleOfThree"))
+			button.action = #selector(showApp(_:))
+		}
+		popover.contentViewController = ViewController.freshController()
+	}
+	@objc func showApp(_ sender: Any?) {
+		if popover.isShown {
+			closePopover(sender: sender)
+		} else {
+			showPopover(sender: sender)
+		}
+	}
+	
+	func showPopover(sender: Any?) {
+		if let button = statusItem.button {
+			popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+		}
+	}
+	
+	func closePopover(sender: Any?) {
+		popover.performClose(sender)
 	}
 
 	func applicationWillTerminate(_ aNotification: Notification) {
 		// Insert code here to tear down your application
 	}
-
-
 }
 
--- Rule of Three/ViewController.swift
+++ Rule of Three/ViewController.swift
@@ -9,19 +9,62 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+	enum rule {
+		case direct
+		case inverse
+	}
+	// Variables:
+	var currentRule = rule.direct
+	
+	@IBOutlet weak var ruleButton: NSButton!
+	
+	@IBOutlet weak var firstIf: NSTextField!
+	@IBOutlet weak var firstIs: NSTextField!
+	@IBOutlet weak var secondIf: NSTextField!
+	@IBOutlet weak var secondIs: NSTextField!
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
-		// Do any additional setup after loading the view.
+		NSEvent.addLocalMonitorForEvents(matching: .keyUp) {
+			print("key up")
+			return $0
+		}
+		
 	}
-
+	override func viewWillAppear() {
+		view.window?.makeFirstResponder(firstIf)
+		print("view will appear")
+	}
 	override var representedObject: Any? {
 		didSet {
 		// Update the view, if already loaded.
 		}
 	}
 
-
+	@IBAction func changeRule(_ sender: Any) {
+		if currentRule == .direct {
+			currentRule = .inverse
+			ruleButton.image = NSImage(named: NSImage.Name(rawValue: "inverse"))
+		} else {
+			currentRule = .direct
+			ruleButton.image = NSImage(named: NSImage.Name(rawValue: "direct"))
+		}
+	}
+	
 }
 
+extension ViewController {
+	// MARK: Storyboard instantiation
+	static func freshController() -> ViewController {
+		//1.
+		let storyboard = NSStoryboard(name: NSStoryboard.Name(rawValue: "Main"), bundle: nil)
+		//2.
+		let identifier = NSStoryboard.SceneIdentifier(rawValue: "ViewController")
+		//3.
+		guard let viewcontroller = storyboard.instantiateController(withIdentifier: identifier) as? ViewController else {
+			fatalError("Why cant i find QuotesViewController? - Check Main.storyboard")
+		}
+		return viewcontroller
+	}
+}
